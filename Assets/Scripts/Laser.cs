ebf9d920997e21b7e28d92de6336117cc63a0819
Kohei <andokohei@gmail.com>
2020-10-19 16:27:37 +0900

    Add a Laser script to move them up

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+  private float _speed = 8.0f;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Time.deltaTime: time between frames of FPS
+    transform.Translate(Vector3.up * _speed * Time.deltaTime);
+  }
+}
