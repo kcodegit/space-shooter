855f4f35dc9b1ba7fdeef6b9535e33c7630157fd
Kohei <andokohei@gmail.com>
2020-10-23 17:27:21 +0900

    Add enemy container

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
   [SerializeField]
   private GameObject _enemyPrefab;
+  [SerializeField]
+  private GameObject _enemyContainer;
 
   // Start is called before the first frame update
   void Start()
@@ -25,7 +27,8 @@ public class SpawnManager : MonoBehaviour
     while (true)
     {
       Vector3 position = new Vector3(Random.Range(-8f, 8f), 7, 0);
-      Instantiate(_enemyPrefab, position, Quaternion.identity);
+      GameObject newEnemy = Instantiate(_enemyPrefab, position, Quaternion.identity);
+      newEnemy.transform.parent = _enemyContainer.transform;
       yield return new WaitForSeconds(5.0f);
     }
   }
