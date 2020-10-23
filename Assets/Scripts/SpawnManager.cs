64704716302e05923bfecde9e16a481eae0353f0
Kohei <andokohei@gmail.com>
2020-10-23 17:24:14 +0900

    Implement spawning enemy object functionality

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [SerializeField]
+  private GameObject _enemyPrefab;
 
-    // Update is called once per frame
-    void Update()
+  // Start is called before the first frame update
+  void Start()
+  {
+    StartCoroutine(SpawnEnemyRoutine());
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  // spawn game object every 5 seconds
+  IEnumerator SpawnEnemyRoutine()
+  {
+    while (true)
     {
-        
+      Vector3 position = new Vector3(Random.Range(-8f, 8f), 7, 0);
+      Instantiate(_enemyPrefab, position, Quaternion.identity);
+      yield return new WaitForSeconds(5.0f);
     }
+  }
+
 }
