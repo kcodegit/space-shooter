d924ecbc4bfaec0ab46261ebc9d40f5a5cf1970e
Kohei <andokohei@gmail.com>
2020-11-09 16:22:00 +0900

    Remove unused code

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
   private float _speed = 8.0f;
-  // Start is called before the first frame update
-  void Start()
-  {
-
-  }
-
-  // Update is called once per frame
+  
   void Update()
   {
     // Time.deltaTime: time between frames of FPS
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -14,17 +14,10 @@ public class SpawnManager : MonoBehaviour
   private GameObject _itemContainer;
   private bool _stopSpawning = false;
 
-  // Start is called before the first frame update
   void Start()
   {
     StartCoroutine(SpawnEnemyRoutine());
     StartCoroutine(SpawnTripleShotPURoutine());
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
   }
 
   // spawn game object every 5 seconds
