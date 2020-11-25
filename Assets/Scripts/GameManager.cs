217c9f3b11539266a2fce413988b7d5094a3cedd
Kohei <andokohei@gmail.com>
2020-11-25 14:40:22 +0900

    Implement game quit trigger

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,12 @@ public class GameManager : MonoBehaviour
     {
       SceneManager.LoadScene(1);
     }
+
+    // Quit the game
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+      Application.Quit();
+    }
   }
 
   public void GameOver()
